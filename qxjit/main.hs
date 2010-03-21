ba63ca865b2ef0c3655ed094238eebb1e530e1d1
qxjit (David Vollbracht) <david.vollbracht@gmail.com>
2010-03-21 10:30:13 -0400

    do problem 10

--- qxjit/main.hs
+++ qxjit/main.hs
@@ -7,19 +7,23 @@ import qualified Problem6 as Problem6
 import qualified Problem7 as Problem7
 import qualified Problem8 as Problem8
 import qualified Problem9Fast as Problem9Fast
+import qualified Problem10 as Problem10
 
 import Prompt
+import Control.Monad (forever)
 
 main = do
   n <- prompt "Problem Number"
-  case n of
-    1 -> Problem1.main
-    2 -> Problem2.main
-    3 -> Problem3.main
-    4 -> Problem4.main
-    5 -> Problem5.main
-    6 -> Problem6.main
-    7 -> Problem7.main
-    8 -> Problem8.main
-    9 -> Problem9Fast.main
-    otherwise -> putStrLn "Unknown Problem" >> main
+  forever $ do
+    case n of
+      1 -> Problem1.main
+      2 -> Problem2.main
+      3 -> Problem3.main
+      4 -> Problem4.main
+      5 -> Problem5.main
+      6 -> Problem6.main
+      7 -> Problem7.main
+      8 -> Problem8.main
+      9 -> Problem9Fast.main
+      10 -> Problem10.main
+      otherwise -> putStrLn "Unknown Problem" >> main
